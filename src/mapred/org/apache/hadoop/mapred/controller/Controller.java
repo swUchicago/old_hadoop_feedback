81ecaa3dc39fcddb6f46b9fb528a0ee64461e6bc
williamsentosa <williamsentosa.sentosa@gmail.com>
2017-07-15 19:11:58 +0700

    change controller

--- src/mapred/org/apache/hadoop/mapred/controller/Controller.java
+++ src/mapred/org/apache/hadoop/mapred/controller/Controller.java
@@ -8,10 +8,15 @@ public class Controller {
     // Attributes
     private static Controller instance = new Controller();
     public static final int DEFAULT_TARGET = 2;
+    public static final int DEFAULT_VIRTUAL_TARGET = 1;
     private int target;
+    private int virtualTarget;
+    private long previous_minspacestart;
 
     private Controller() {
         target = DEFAULT_TARGET;
+        virtualTarget = DEFAULT_VIRTUAL_TARGET;
+        previous_minspacestart = 0;
     }
 
     public static Controller getInstance() {
@@ -19,8 +24,20 @@ public class Controller {
     }
 
     public long calculateMinspacestart(int currentMaxExceptions, int mapParallelism, long intermediateFileSize) {
-        double result;
-        result = ((double) currentMaxExceptions / (double) target) * mapParallelism * intermediateFileSize;
+        double result, a, p, p1, p2;
+        p1 = 1.5;
+        p2 = 3;
+        a = 1 / 157286400;
+        if (currentMaxExceptions <= virtualTarget) {
+            p = p1;
+        } else {
+            p = p2;
+        }
+        result = previous_minspacestart + (1 - p) / a * (virtualTarget - currentMaxExceptions);
+        if (result < 0) {
+            result = 0;
+        }
+        previous_minspacestart = (long) result;
         return (long) result;
     }
 
